9af258a5b8465ff464329431e21aa3d73aea4005
Denis Bernard <db047h@gmail.com>
2019-06-08 16:49:17 +0200

    Add tests for new PRNGs and update reference implementations.

--- refimpl/xoroshiro128starstar.c
+++ refimpl/xoroshiro128starstar.c
@@ -1,4 +1,4 @@
-/*  Written in 2016-2018 by David Blackman and Sebastiano Vigna (vigna@acm.org)
+/*  Written in 2018 by David Blackman and Sebastiano Vigna (vigna@acm.org)
 
 To the extent possible under law, the author has dedicated all copyright
 and related and neighboring rights to this software to the public domain
@@ -8,28 +8,16 @@ See <http://creativecommons.org/publicdomain/zero/1.0/>. */
 
 #include <stdint.h>
 
-/* This is xoroshiro128+ 1.0, our best and fastest small-state generator
-   for floating-point numbers. We suggest to use its upper bits for
-   floating-point generation, as it is slightly faster than
-   xoroshiro128**. It passes all tests we are aware of except for the four
-   lower bits, which might fail linearity tests (and just those), so if
-   low linear complexity is not considered an issue (as it is usually the
-   case) it can be used to generate 64-bit outputs, too; moreover, this
-   generator has a very mild Hamming-weight dependency making our test
-   (http://prng.di.unimi.it/hwd.php) fail after 5 TB of output; we believe
-   this slight bias cannot affect any application. If you are concerned,
-   use xoroshiro128** or xoshiro256+.
+/* This is xoroshiro128** 1.0, our all-purpose, rock-solid, small-state
+   generator. It is extremely (sub-ns) fast and it passes all tests we are
+   aware of, but its state space is large enough only for mild parallelism.
 
-   We suggest to use a sign test to extract a random Boolean value, and
-   right shifts to extract subsets of bits.
+   For generating just floating-point numbers, xoroshiro128+ is even
+   faster (but it has a very mild bias, see notes in the comments).
 
    The state must be seeded so that it is not everywhere zero. If you have
    a 64-bit seed, we suggest to seed a splitmix64 generator and use its
-   output to fill s. 
-
-   NOTE: the parameters (a=24, b=16, b=37) of this version give slightly
-   better results in our test than the 2016 version (a=55, b=14, c=36).
-*/
+   output to fill s. */
 
 static inline uint64_t rotl(const uint64_t x, int k)
 {
@@ -42,7 +30,7 @@ uint64_t next(void)
 {
 	const uint64_t s0 = s[0];
 	uint64_t s1 = s[1];
-	const uint64_t result = s0 + s1;
+	const uint64_t result = rotl(s0 * 5, 7) * 9;
 
 	s1 ^= s0;
 	s[0] = rotl(s0, 24) ^ s1 ^ (s1 << 16); // a, b
